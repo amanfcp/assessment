fab2b1d74e0007c1552509dcec88226ada21faaf
Aman Ullah Jalal <amanullahjalal@folio3.com>
2024-05-07 21:20:07 +0500

    refactor: added class comments in util function

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,13 @@
 import {Category, HierarchicalCategory} from '../types';
 
+/**
+ * Recursively builds a hierarchical category tree from a flat list of categories.
+ * Each category can have sub-categories, forming a tree structure.
+ *
+ * @param {Category[]} categories - The flat list of categories to be transformed into a tree.
+ * @param {number | null} parentId - The ID of the parent category. Use null for the root categories.
+ * @returns {HierarchicalCategory[]} - The hierarchical category tree.
+ */
 export function buildCategoryTree(
   categories: Category[],
   parentId: number | null,
